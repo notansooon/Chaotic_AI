39c7a17da1c3bf4ac75a6c3186c641070c57dc8b
notansooon <ansonjiang043@gmail.com>
2025-11-20 19:01:49 -0500

    c++ core integration

---
+++ database/runtime/native/agent_core/include/kyntrix/agent_core.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    const char* ingest_url;
+    const char* run_id;
+    int         batch_size;
+    int         flush_interval_ms;
+}   kyntrixAgentConfig
+
+typedef struct {
+    const char* run_id;
+    long long 
+}
+} 
